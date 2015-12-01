3734d6c851f370a085c0be16d2b66f6a91008a5b
Xiulan Shi <xiulanshi2013@gmail.com>
2015-11-30 20:20:13 -0500

    First Commit

---
+++ Santa/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Santa
+//
+
+//  Copyright (c) 2015 Xiulan Shi. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Santa/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Santa
+//
+
+//  Copyright (c) 2015 Xiulan Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
